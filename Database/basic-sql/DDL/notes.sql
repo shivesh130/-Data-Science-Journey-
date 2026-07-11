9b3531451550eaddffd0374f7228ccfdf07740bc
Shivesh Tiwari <shivesht066@gmail.com>
2026-07-11 19:25:21 +0530

    sql ddl added

--- Database/basic-sql/DDL/notes.sql
+++ Database/basic-sql/DDL/notes.sql
@@ -1,38 +1,47 @@
 -- DDL :- Data definition language
 -- create, alter, drop, truncate, rename
-create database employee;
-use employee;
-create table employee(
+create database DDL;
+use DDL;
+create table table1(
     emp_id int primary key,
     emp_name varchar(50) not null,
     emp_salary decimal(10,2));
-insert into employee values(1,'rahul',50000.00);
-insert into employee values(2,'shivesh',60000.00);    
-select*from employee;
-truncate table employee;
-select*from employee;
-insert into employee values(3,'anjali',40000.00),(4,'sumant',30000.00),(5,'sachin',70000.00),(6,'rohit',80000.00), (7,'virat',90000.00);
+insert into table1 values(101,'rahul',50000.00);
+insert into table1 values(102,'shivesh',60000.00);    
+select*from table1;
+truncate table table1;
+select*from table1;
+insert into table1 values(1,'anjali',40000.00),(2,'sumant',30000.00),(3,'sachin',70000.00),(4,'rohit',80000.00), (5,'virat',90000.00);
 
+select*from table1;
 
 -- alter   
 
+
 -- new column
-alter table employee add employee_address varchar(100);
-select*from employee;
+alter table table1 add employee_address varchar(100);
+select*from table1;
 
 -- column delete
-alter table employee drop column employee_address;
-select*from employee;
+alter table table1 drop column employee_address;
+select*from table1;
 
 -- column property change
-desc employee;
-alter table employee modify column emp_name varchar(100);
-desc employee;
+desc table1;
+alter table table1 modify column emp_name varchar(100);
+desc table1;
 
 -- rename table
-alter table employee rename to emp;
-select*from emp;
+alter table table1 rename to table2;
+select*from table2;
 
 -- column change
-alter table emp change column emp_salary emp_salary decimal(12,2);
-desc emp;
+alter table table2 change column emp_salary emp_salary decimal(12,2);
+desc table2;
+
+select*from table2;
+alter table table2 rename to table1;
+
+select*from table1;
+
+
